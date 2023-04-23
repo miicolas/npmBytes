d4fb62996df444c7f6e5cf246b5f30a6ff7a263a
Nicolas Becharat <nicolas.becharat@gmail.com>
2023-04-23 16:54:40 +0200

    post card

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
 import { defineConfig } from 'astro/config';
-
 import mdx from "@astrojs/mdx";
+
+import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx()]
+  integrations: [mdx(), image()]
 });
--- src/js/utils.js
+++ src/js/utils.js
@@ -1,13 +1,15 @@
 export function slugify(text) {
-    return text
-      .toString()
-      .toLowerCase()
-      .replace(/\s+/g, '-')
-      .replace(/[^\w-]+/g, '')
-      .replace(/--+/g, '-')
-      .replace(/^-+/, '')
-      .replace(/-+$/, '');
-  }
+  if (!text) return "";
+  return text
+    .toString()
+    .toLowerCase()
+    .replace(/\s+/g, "-")
+    .replace(/[^\w\-]+/g, "")
+    .replace(/\-\-+/g, "-")
+    .replace(/^-+/, "")
+    .replace(/-+$/, "");
+}
+
   
   export function formatDate(date) {
     return new Date(date).toLocaleDateString('fr-EU', {
