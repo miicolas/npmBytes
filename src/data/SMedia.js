8e95616c445e83756bb5a2f6bb689fd4f51098cf
Nicolas Becharat <78233186+justniicolas@users.noreply.github.com>
2023-04-27 00:00:31 +0200

    ✨ add links social media

---
+++ src/data/SMedia.js
@@ -0,0 +1,13 @@
+const SMedia = [
+    {
+         name : "Instragram",
+         url: "https://www.instagram.com/useflow.dev",
+    },
+    {
+        name : "Mail",
+        url: "mailto:contact.useflow@gmail.com",
+   }
+    
+]
+
+export default SMedia;
