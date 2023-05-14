a34e82cdea1697ec227ed6f2fc241320b052633d
Nicolas Becharat <78233186+justniicolas@users.noreply.github.com>
2023-05-14 21:31:50 +0200

    ✨ add carrousel

---
+++ src/data/CarousselSlide.js
@@ -0,0 +1,50 @@
+const CarousselSlide = [
+    {
+        description : "Subscribe",
+        path : "/images/flow.png",
+    },
+    {
+        description : "Subscribe",
+        path : "/images/flow.png",
+    },
+    {
+        description : "Subscribe",
+        path : "/images/flow.png",
+    },
+    {
+        description : "Subscribe",
+        path : "/images/flow.png",
+    },
+    {
+        description : "Subscribe",
+        path : "/images/flow.png",
+    },
+    {
+        description : "Subscribe",
+        path : "/images/flow.png",
+    },
+    {
+        description : "Subscribe",
+        path : "/images/flow.png",
+    },
+    {
+        description : "Subscribe",
+        path : "/images/flow.png",
+    },
+    {
+        description : "Subscribe",
+        path : "/images/flow.png",
+    },
+    {
+        description : "Subscribe",
+        path : "/images/flow.png",
+    },
+        {
+        description : "Subscribe",
+        path : "/images/flow.png",
+    },
+
+    
+]
+
+export default CarousselSlide;
