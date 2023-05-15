4068a7c78693edcc783b772b08087990bd562283
Nicolas Becharat <nicolas.becharat@gmail.com>
2023-05-15 20:58:08 +0200

    ⚙️ update carrousel

--- src/data/CarousselSlide.js
+++ src/data/CarousselSlide.js
@@ -21,7 +21,7 @@ const CarousselSlide = [
   },
   {
     description: "Subscribe",
-    path: "https://cdn-icons-png.flaticon.com/512/5968/5968509.png",
+    path: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/97/Netlify_logo_%282%29.svg/1200px-Netlify_logo_%282%29.svg.png",
   },
   {
     description: "Subscribe",
