5e8d2a210704dd10354a8d78b01c319e288fd21f
Nicolas Becharat <nicolas.becharat@gmail.com>
2023-05-15 20:44:42 +0200

    ⚙️⚙️⚙️

--- src/data/CarousselSlide.js
+++ src/data/CarousselSlide.js
@@ -5,7 +5,15 @@ const CarousselSlide = [
   },
   {
     description: "Subscribe",
-    path: "https://cdn-icons-png.flaticon.com/512/5968/5968509.png",
+    path: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/97/Netlify_logo_%282%29.svg/1200px-Netlify_logo_%282%29.svg.png",
+  },
+  {
+    description: "Subscribe",
+    path: "/images/npmbytes.png",
+  },
+  {
+    description: "Subscribe",
+    path: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/97/Netlify_logo_%282%29.svg/1200px-Netlify_logo_%282%29.svg.png",
   },
   {
     description: "Subscribe",
@@ -21,7 +29,7 @@ const CarousselSlide = [
   },
   {
     description: "Subscribe",
-    path: "https://cdn-icons-png.flaticon.com/512/5968/5968509.png",
+    path: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/97/Netlify_logo_%282%29.svg/1200px-Netlify_logo_%282%29.svg.png",
   },
   {
     description: "Subscribe",
@@ -29,7 +37,7 @@ const CarousselSlide = [
   },
   {
     description: "Subscribe",
-    path: "https://cdn-icons-png.flaticon.com/512/5968/5968509.png",
+    path: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/97/Netlify_logo_%282%29.svg/1200px-Netlify_logo_%282%29.svg.png",
   },
   {
     description: "Subscribe",
@@ -37,15 +45,7 @@ const CarousselSlide = [
   },
   {
     description: "Subscribe",
-    path: "https://cdn-icons-png.flaticon.com/512/5968/5968509.png",
-  },
-  {
-    description: "Subscribe",
-    path: "/images/npmbytes.png",
-  },
-  {
-    description: "Subscribe",
-    path: "https://cdn-icons-png.flaticon.com/512/5968/5968509.png",
+    path: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/97/Netlify_logo_%282%29.svg/1200px-Netlify_logo_%282%29.svg.png",
   },
   {
     description: "Subscribe",
