aae78dfa8465cdff3af583be328fb9ee3cd56474
Nicolas Becharat <nicolas.becharat@gmail.com>
2023-05-15 20:59:14 +0200

    Update CarousselSlide.js

--- src/data/CarousselSlide.js
+++ src/data/CarousselSlide.js
@@ -5,7 +5,7 @@ const CarousselSlide = [
   },
   {
     description: "Subscribe",
-    path: "https://cdn-icons-png.flaticon.com/512/5968/5968509.png",
+    path: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/97/Netlify_logo_%282%29.svg/1200px-Netlify_logo_%282%29.svg.png",
   },
   {
     description: "Subscribe",
@@ -13,7 +13,7 @@ const CarousselSlide = [
   },
   {
     description: "Subscribe",
-    path: "https://cdn-icons-png.flaticon.com/512/5968/5968509.png",
+    path: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/97/Netlify_logo_%282%29.svg/1200px-Netlify_logo_%282%29.svg.png",
   },
   {
     description: "Subscribe",
@@ -21,7 +21,7 @@ const CarousselSlide = [
   },
   {
     description: "Subscribe",
-    path: "https://cdn-icons-png.flaticon.com/512/5968/5968509.png",
+    path: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/97/Netlify_logo_%282%29.svg/1200px-Netlify_logo_%282%29.svg.png",
   },
   {
     description: "Subscribe",
@@ -29,7 +29,7 @@ const CarousselSlide = [
   },
   {
     description: "Subscribe",
-    path: "https://cdn-icons-png.flaticon.com/512/5968/5968509.png",
+    path: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/97/Netlify_logo_%282%29.svg/1200px-Netlify_logo_%282%29.svg.png",
   },
   {
     description: "Subscribe",
@@ -37,7 +37,7 @@ const CarousselSlide = [
   },
   {
     description: "Subscribe",
-    path: "https://cdn-icons-png.flaticon.com/512/5968/5968509.png",
+    path: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/97/Netlify_logo_%282%29.svg/1200px-Netlify_logo_%282%29.svg.png",
   },
   {
     description: "Subscribe",
@@ -45,7 +45,7 @@ const CarousselSlide = [
   },
   {
     description: "Subscribe",
-    path: "https://cdn-icons-png.flaticon.com/512/5968/5968509.png",
+    path: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/97/Netlify_logo_%282%29.svg/1200px-Netlify_logo_%282%29.svg.png",
   },
   {
     description: "Subscribe",
