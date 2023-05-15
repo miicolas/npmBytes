073d3d86bbc791cd89419972513766e55b641ba0
Nicolas Becharat <nicolas.becharat@gmail.com>
2023-05-15 21:08:55 +0200

    ⚙️⚙️⚙️

--- src/data/CarousselSlide.js
+++ src/data/CarousselSlide.js
@@ -51,6 +51,18 @@ const CarousselSlide = [
     description: "Subscribe",
     path: "/images/npmbytes.png",
   },
+  {
+    description: "Subscribe",
+    path: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/97/Netlify_logo_%282%29.svg/1200px-Netlify_logo_%282%29.svg.png",
+  },
+  {
+    description: "Subscribe",
+    path: "/images/npmbytes.png",
+  },
+  {
+    description: "Subscribe",
+    path: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/97/Netlify_logo_%282%29.svg/1200px-Netlify_logo_%282%29.svg.png",
+  },
 ];
 
 export default CarousselSlide;
