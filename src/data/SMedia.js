ceef17bfa91ffeda1ea5899667d4d7939bcf57be
Nicolas Becharat <nicolas.becharat@gmail.com>
2023-05-19 19:35:27 +0200

    ✨ post 1 to 9

--- src/data/SMedia.js
+++ src/data/SMedia.js
@@ -1,13 +1,13 @@
 const SMedia = [
-    {
-         name : "Instragram",
-         url: "https://www.instagram.com/npmbytes",
-    },
-    {
-        name : "Mail",
-        url: "mailto:contact.npmbytes@gmail.com",
-   }
-    
-]
+  {
+    name: "Instragram",
+    url: "https://www.instagram.com/npmbytes",
+  },
+  {
+    name: "Mail",
+    url: "mailto:contact.npmbytes@gmail.com",
+  },
+
+];
 
 export default SMedia;
