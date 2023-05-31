9c456a21c055c81686523ae915c649b9181cbf97
Nicolas Becharat <78233186+justniicolas@users.noreply.github.com>
2023-05-31 20:54:08 +0200

    Update client.ts

---
+++ src/firebase/client.ts
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAKm8cYPFpAJ-hUVAmJ6D1LYCAXK_85-u0",
+    authDomain: "betanpmbytes.firebaseapp.com",
+    projectId: "betanpmbytes",
+    storageBucket: "betanpmbytes.appspot.com",
+    messagingSenderId: "660574342874",
+    appId: "1:660574342874:web:a0c4d8e0be0102163e9fe3",
+    measurementId: "G-RCGV3TW4T9"
+  };
+
+export const app = initializeApp(firebaseConfig);
