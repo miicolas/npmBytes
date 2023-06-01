49de9ad14c8b111a93bd4c3d18ac9209490196cb
Nicolas Becharat <78233186+justniicolas@users.noreply.github.com>
2023-06-01 16:42:09 +0200

    ⚙️

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,14 @@ import mdx from "@astrojs/mdx";
 import { remarkReadingTime } from './src/js/remark-reading-time.mjs';
 import image from "@astrojs/image";
 
-
+import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
-  //output: 'server',
+  output: 'server',
   integrations: [mdx({
     drafts: true
   }), image()],
   remarkPlugins: [remarkReadingTime],
+  adapter: netlify()
 });
