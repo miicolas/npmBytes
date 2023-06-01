fb5a3ffa220fb0d70dd8a7ea317e1fd45849fe17
Nicolas Becharat <78233186+justniicolas@users.noreply.github.com>
2023-06-01 23:04:36 +0200

    ⚙️

---
+++ src/pages/api/subscribe.ts
@@ -0,0 +1,26 @@
+import type { APIRoute } from "astro";
+import { app } from "../../firebase/server";
+import { getFirestore } from "firebase-admin/firestore";
+
+export const post: APIRoute = async ({ request, redirect }) => {
+  const formData = await request.formData();
+  const email = formData.get("email")?.toString();
+
+  if (!email) {
+    return new Response("Missing required fields", {
+      status: 400,
+    });
+  }
+  try {
+    const db = getFirestore(app);
+    const subscribeRef = db.collection("subscribe");
+    await subscribeRef.add({
+      email
+    });
+  } catch (error) {
+    return new Response("Something went wrong", {
+      status: 500,
+    });
+  }
+  return redirect("/");
+};
