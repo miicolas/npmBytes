afd161d8a31edb6bac623ecc24938f99a33ea96d
Nicolas Becharat <78233186+justniicolas@users.noreply.github.com>
2023-06-02 18:39:23 +0200

    Update env.d.ts

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,13 +1,13 @@
 interface ImportMetaEnv {
-    readonly FIREBASE_PRIVATE_KEY_ID: string;
-    readonly FIREBASE_PRIVATE_KEY: string;
-    readonly FIREBASE_PROJECT_ID: string;
-    readonly FIREBASE_CLIENT_EMAIL: string;
-    readonly FIREBASE_CLIENT_ID: string;
-    readonly FIREBASE_AUTH_URI: string;
-    readonly FIREBASE_TOKEN_URI: string;
-    readonly FIREBASE_AUTH_CERT_URL: string
-    readonly FIREBASE_CLIENT_CERT_URL: string;
+    readonly VITE_FIREBASE_PRIVATE_KEY_ID: string;
+    readonly VITE_FIREBASE_PRIVATE_KEY: string;
+    readonly VITE_FIREBASE_PROJECT_ID: string;
+    readonly VITE_FIREBASE_CLIENT_EMAIL: string;
+    readonly VITE_FIREBASE_CLIENT_ID: string;
+    readonly VITE_FIREBASE_AUTH_URI: string;
+    readonly VITE_FIREBASE_TOKEN_URI: string;
+    readonly VITE_FIREBASE_AUTH_CERT_URL: string
+    readonly VITE_FIREBASE_CLIENT_CERT_URL: string;
   }
   
   interface ImportMeta {
