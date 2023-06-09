239f424610edb678452f0b0558ffd820d04caa64
Nicolas Becharat <78233186+justniicolas@users.noreply.github.com>
2023-06-09 20:01:58 +0200

    ✨ add tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,15 +2,16 @@ import { defineConfig } from 'astro/config';
 import mdx from "@astrojs/mdx";
 import { remarkReadingTime } from './src/js/remark-reading-time.mjs';
 import image from "@astrojs/image";
-
 import netlify from "@astrojs/netlify/functions";
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
   integrations: [mdx({
     drafts: true
-  }), image()],
+  }), image(), tailwind()],
   remarkPlugins: [remarkReadingTime],
   adapter: netlify()
 });
