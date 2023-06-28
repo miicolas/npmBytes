1862d3832fd330d56c38afdf361cac0947bdcae3
Nicolas Becharat <78233186+justniicolas@users.noreply.github.com>
2023-06-28 23:44:41 +0200

    ✨ i18n system

---
+++ src/i18n/ui.ts
@@ -0,0 +1,21 @@
+export const languages = {
+    en: 'English',
+    fr: 'Français',
+  };
+  
+  export const defaultLang = 'en';
+  
+  export const ui = {
+    en: {
+      'nav.home': 'Home',
+      'nav.instruction': 'To access the navigation menu, please press',
+      'nav.twitter': 'Twitter',
+      'home.slogan': 'All the digital core in one media'
+    },
+    fr: {
+      'nav.home': 'Accueil',
+      'nav.instruction': 'Pour accéder au menu de navigation, veuillez appuyer sur',
+      'home.slogan' : 'Tout le cœur numérique en un seul support'
+      
+    },
+  } as const;
