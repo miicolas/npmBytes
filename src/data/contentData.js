6f1f8e52c7da3ddd28a7a215feefd56580a998d7
Nicolas Becharat <78233186+justniicolas@users.noreply.github.com>
2023-06-30 23:11:51 +0200

    ✨ i18n

--- src/data/contentData.js
+++
@@ -1,9 +0,0 @@
-const contentData = [
-    {
-      name : "Privacy Policy", 
-      url : "content/privacy-policy"
-    }
-  ];
-  
-  export default contentData;
-  
--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -19,7 +19,13 @@ export const languages = {
       'home.button-more' : 'See more',
       'blog.slogan' : 'Unlock a treasure trove of knowledge',
       'blog.readtime' : 'minutes read',
-      'footer.rights' : 'All Rights Reserved'
+      'footer.rights' : 'All Rights Reserved',
+      'content.privacy' : 'Privacy Content',
+      'content.layout-update' : 'Last update', 
+      'unsubscribe.from-title' : 'Unsubscribe form',
+      'unsubcribe.txt1' : "To unsubscribe, please fill out the form below, and we will promptly take the necessary steps to remove your email address from our mailing list.Please note that once your unsubscription is processed, you will no longer receive our communications, updates, and special offers. However, if you change your mind in the future, you are always welcome to re-subscribe at any time.We would like to sincerely thank you for your past interest in our newsletter. If you have any feedback or suggestions on how we could improve our content, please feel free to let us know. Best regards," , 
+      'unsubscribe.team' : "npmBytes Team", 
+      'recommandation.title' : 'Explore these recommended articles to dive deeper into your reading!'
     },
     fr: {
       'nav.home': 'Accueil',
@@ -34,6 +40,12 @@ export const languages = {
       'blog.slogan' : 'Débloquez un trésor de connaissances', 
       'blog.readtime' : 'minutes de lecture',
       'footer.rights' : 'Tous droits réservés',
+      'content.privacy' : 'Politique de confidentialité', 
+      'content.layout-update' : 'Dernière modification' ,
+      'unsubscribe.form-title' : 'Formulaire de désabonnement',
+      'unsubscribe.txt1' : "Pour vous désabonner, veuillez remplir le formulaire ci-dessous, et nous prendrons rapidement les mesures nécessaires pour supprimer votre adresse e-mail de notre liste de diffusion.Veuillez noter qu'une fois votre désinscription traitée, vous ne recevrez plus nos communications, mises à jour et offres spéciales. Cependant, si vous changez d'avis à l'avenir, vous pouvez toujours vous réabonner à tout moment.Nous tenons à vous remercier sincèrement pour l'intérêt que vous avez porté à notre newsletter. Si vous avez des commentaires ou des suggestions sur la façon dont nous pourrions améliorer notre contenu, n'hésitez pas à nous le faire savoir. Cordialement," , 
+      'unsubscribe.team' : "L'équipe npmBytes", 
+      'recommandation.title' : 'Explorez ces articles recommandés pour approfondir votre lecture !'
 
       
     },
