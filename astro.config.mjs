27b7c5811448df22a51bf353643289f5cd7eaab1
Nicolas Becharat <nicolas.becharat@gmail.com>
2023-08-26 08:35:29 +0200

    vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,20 +2,19 @@ import { defineConfig } from 'astro/config';
 import mdx from "@astrojs/mdx";
 import { remarkReadingTime } from './src/js/remark-reading-time.mjs';
 import image from "@astrojs/image";
-import netlify from "@astrojs/netlify/functions";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   experimental: {
     viewTransitions: true
-   },
+  },
   output: 'server',
   integrations: [mdx({
     drafts: true
   }), image(), tailwind(), react()],
   remarkPlugins: [remarkReadingTime],
-  adapter: netlify()
-  
+  adapter: vercel()
 });
